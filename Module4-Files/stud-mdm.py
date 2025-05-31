7cc1e0fe9f0b35549fadabc213b7fbf951398078
MDMilano <marcdanielmilano@gmail.com>
2025-05-31 17:05:05 +0800

    module 4

--- Module4-Files/stud-mdm.py
+++ Module4-Files/stud-mdm.py
@@ -5,52 +5,53 @@ from flask import flash, render_template, request, redirect
 
 @app.route('/')
 def home():
-    return read_all_user()
+    return read_all_stud()
 
 #View Read-All Page
-@app.route('/read_all_user')
-def read_all_user():
+@app.route('/read_all_stud')
+def read_all_stud():
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user_info")
+        cursor.execute("SELECT * FROM tbl_stud_info")
         rows = cursor.fetchall()
-        return render_template('readusers.html', rows=rows)
+        return render_template('readstuds.html', rows=rows)
     except Exception as e:
         print(e)
     finally:
         cursor.close()
         conn.close()
 
-#View Create New User Page
-@app.route('/create_user_view')
-def create_user_view():
-    return render_template('addusers.html')
+#View Create New Student Page
+@app.route('/create_stud_view')
+def create_stud_view():
+    return render_template('addstuds.html')
 
-#POST METHOD to Save New User Record
-@app.route('/create_user', methods=['POST'])
-def create_user():
+#POST METHOD to Save New Student Record
+@app.route('/create_stud', methods=['POST'])
+def create_stud():
     conn = None
     cursor = None
     try:
-        #GET USER INPUTS
-        strMDMID = request.form['UserID']
+        #GET STUDENT INPUTS
+        strMDMID = request.form['StudentID']
         strMDMLName = request.form['LastName']
         strMDMFName = request.form['FirstName']
         strMDMMName = request.form['MiddleName']
+        strMDMgender = request.form['gender']
+        strMDMBirthday = request.form['Birthday']
+        strMDMAge = request.form['Age']
+        strMDMCourse = request.form['Course']
         strMDMHomeAdd = request.form['Address']
         strMDMEmailAdd = request.form['EmailAdd']
-        strMDMMobNo = request.form['MobileNo']
-        strMDMUType = request.form['usertype']
-        strMDMStatus = request.form['status']
-        strMDMPassword = 'default'
+
 
         if request.method == 'POST':
             #SAVE RECORD TO DATABASE
-            sql = "INSERT INTO tbl_user_info \
-                    (User_ID, Last_Name, First_Name, Middle_Name, Home_Address, \
+            sql = "INSERT INTO tbl_stud_info \
+                    (Student_ID, Last_Name, First_Name, Middle_Name, Home_Address, \
                     Email_Address, Mobile_Number,User_Type,Status, Password) \
                     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             data = (strMDMID, strMDMLName, strMDMFName, strMDMMName, strMDMHomeAdd, strMDMEmailAdd,
@@ -60,9 +61,9 @@ def create_user():
             cursor.execute(sql, data)
             conn.commit()
             flash('Record successfully saved in the database!')
-            return redirect('/read_all_user')
+            return redirect('/read_all_stud')
         else:
-            return 'Error while adding user'
+            return 'Error while adding student'
     except Exception as e:
         print(e)
     finally:
@@ -70,17 +71,17 @@ def create_user():
         conn.close()
 
 #View Read-One Page
-@app.route('/read_one_user/<string:id>')
-def read_one_user(id):
+@app.route('/read_one_stud/<string:id>')
+def read_one_stud(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user_info WHERE user_id=%s", id)
+        cursor.execute("SELECT * FROM tbl_stud_info WHERE student_id=%s", id)
         row = cursor.fetchone()
         if row:
-            return render_template('viewusers.html', row=row)
+            return render_template('viewstuds.html', row=row)
         else:
             return 'Error loading #{id}'.format(id=id)
     except Exception as e:
@@ -90,17 +91,17 @@ def read_one_user(id):
         conn.close()
 
 #View Update Page
-@app.route('/update_user_view/<string:id>')
-def update_user_view(id):
+@app.route('/update_stud_view/<string:id>')
+def update_stud_view(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user_info WHERE user_id=%s", id)
+        cursor.execute("SELECT * FROM tbl_stud_info WHERE student_id=%s", id)
         row = cursor.fetchone()
         if row:
-            return render_template('editusers.html', row=row)
+            return render_template('editstuds.html', row=row)
         else:
             return 'Error loading #{id}'.format(id=id)
     except Exception as e:
@@ -110,13 +111,13 @@ def update_user_view(id):
         conn.close()
 
 #POST Method to Save Changes in data
-@app.route('/update_user', methods=['POST'])
-def update_user():
+@app.route('/update_stud', methods=['POST'])
+def update_stud():
     conn = None
     cursor = None
     try:
-        # GET USER INPUTS
-        strMDMID = request.form['UserID']
+        # GET STUDENT INPUTS
+        strMDMID = request.form['StudentID']
         strMDMLName = request.form['LastName']
         strMDMFName = request.form['FirstName']
         strMDMMName = request.form['MiddleName']
@@ -128,7 +129,7 @@ def update_user():
 
         if request.method == 'POST':
             # SAVE RECORD TO DATABASE
-            sql = "UPDATE tbl_user_info SET \
+            sql = "UPDATE tbl_stud_info SET \
                     Last_Name=%s, First_Name=%s, Middle_Name=%s, Home_Address=%s, \
                     Email_Address=%s, Mobile_Number=%s, User_Type=%s,Status=%s \
                     WHERE User_ID=%s"
@@ -139,26 +140,26 @@ def update_user():
             cursor.execute(sql, data)
             conn.commit()
             flash('Record successfully edited in the database!')
-            return redirect('/read_all_user')
+            return redirect('/read_all_stud')
         else:
-            return 'Error while updating user'
+            return 'Error while updating student'
     except Exception as e:
         print(e)
     finally:
         cursor.close()
         conn.close()
 
-@app.route('/delete_user/<string:id>')
-def delete_user(id):
+@app.route('/delete_stud/<string:id>')
+def delete_stud(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM tbl_user_info WHERE user_id=%s", (id,))
+        cursor.execute("DELETE FROM tbl_stud_info WHERE student_id=%s", (id,))
         conn.commit()
         flash('Record successfully deleted from the server!')
-        return redirect('/read_all_user')
+        return redirect('/read_all_stud')
     except Exception as e:
         print(e)
     finally:
