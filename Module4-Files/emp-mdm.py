cd7b89e1edb88b2c6d1cd28036a73139ec93533a
MDMilano <marcdanielmilano@gmail.com>
2025-06-02 17:30:48 +0800

    emp 1 mod 1 edited done and emp 2 mod 2 done

--- Module4-Files/emp-mdm.py
+++ Module4-Files/emp-mdm.py
@@ -5,65 +5,65 @@ from flask import flash, render_template, request, redirect
 
 @app.route('/')
 def home():
-    return read_all_stud()
+    return read_all_emp()
 
 #View Read-All Page
-@app.route('/read_all_stud')
-def read_all_stud():
+@app.route('/read_all_emp')
+def read_all_emp():
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_stud_info")
+        cursor.execute("SELECT * FROM tbl_emp_info")
         rows = cursor.fetchall()
-        return render_template('readstuds.html', rows=rows)
+        return render_template('reademps.html', rows=rows)
     except Exception as e:
         print(e)
     finally:
         cursor.close()
         conn.close()
 
-#View Create New Student Page
-@app.route('/create_stud_view')
-def create_stud_view():
-    return render_template('addstuds.html')
+#View Create New Employee Page
+@app.route('/create_emp_view')
+def create_emp_view():
+    return render_template('addemps.html')
 
-#POST METHOD to Save New Student Record
-@app.route('/create_stud', methods=['POST'])
-def create_stud():
+#POST METHOD to Save New Employee Record
+@app.route('/create_emp', methods=['POST'])
+def create_emp():
     conn = None
     cursor = None
     try:
-        #GET STUDENT INPUTS
-        strMDMSID = request.form['StudentID']
+        #GET EMPLOYEE INPUTS
+        strMDMSID = request.form['EmployeeID']
         strMDMLName = request.form['LastName']
         strMDMFName = request.form['FirstName']
         strMDMMName = request.form['MiddleName']
         strMDMGender = request.form['gender']
-        strMDMBirthday = request.form['Birthday']
+        strMDMDateHired = request.form['DateHired']
         strMDMAge = request.form['Age']
-        strMDMCourse = request.form['Course']
+        strMDMDepartment = request.form['Department']
         strMDMHomeAdd = request.form['Address']
         strMDMEmailAdd = request.form['EmailAdd']
 
 
         if request.method == 'POST':
             #SAVE RECORD TO DATABASE
-            sql = "INSERT INTO tbl_stud_info \
-                    (Student_ID, Last_Name, First_Name, Middle_Name, Gender, \
-                    Birthday,Age ,Course ,Home_Address, Email_Address) \
+            sql = "INSERT INTO tbl_emp_info \
+                    (Employee_ID, Last_Name, First_Name, Middle_Name, Gender, \
+                    Date_Hired,Age ,Department ,Home_Address, Email_Address) \
                     VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            data = (strMDMSID, strMDMLName, strMDMFName, strMDMMName, strMDMGender, strMDMBirthday,
-                    strMDMAge, strMDMCourse,strMDMHomeAdd, strMDMEmailAdd)
+            data = (strMDMSID, strMDMLName, strMDMFName, strMDMMName, strMDMGender, strMDMDateHired,
+                    strMDMAge, strMDMDepartment,strMDMHomeAdd, strMDMEmailAdd)
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
             conn.commit()
             flash('Record successfully saved in the database!')
-            return redirect('/read_all_stud')
+            return redirect('/read_all_emp')
         else:
-            return 'Error while adding student'
+            return 'Error while adding employee'
     except Exception as e:
         print(e)
     finally:
@@ -71,17 +71,17 @@ def create_stud():
         conn.close()
 
 #View Read-One Page
-@app.route('/read_one_stud/<string:id>')
-def read_one_stud(id):
+@app.route('/read_one_emp/<string:id>')
+def read_one_emp(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_stud_info WHERE student_id=%s", id)
+        cursor.execute("SELECT * FROM tbl_emp_info WHERE employee_id=%s", id)
         row = cursor.fetchone()
         if row:
-            return render_template('viewstuds.html', row=row)
+            return render_template('viewemps.html', row=row)
         else:
             return 'Error loading #{id}'.format(id=id)
     except Exception as e:
@@ -91,17 +91,17 @@ def read_one_stud(id):
         conn.close()
 
 #View Update Page
-@app.route('/update_stud_view/<string:id>')
-def update_stud_view(id):
+@app.route('/update_emp_view/<string:id>')
+def update_emp_view(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_stud_info WHERE student_id=%s", id)
+        cursor.execute("SELECT * FROM tbl_emp_info WHERE employee_id=%s", id)
         row = cursor.fetchone()
         if row:
-            return render_template('editstuds.html', row=row)
+            return render_template('editemps.html', row=row)
         else:
             return 'Error loading #{id}'.format(id=id)
     except Exception as e:
@@ -111,56 +111,56 @@ def update_stud_view(id):
         conn.close()
 
 #POST Method to Save Changes in data
-@app.route('/update_stud', methods=['POST'])
-def update_stud():
+@app.route('/update_emp', methods=['POST'])
+def update_emp():
     conn = None
     cursor = None
     try:
-        # GET STUDENT INPUTSs
-        strMDMSID = request.form['StudentID']
+        # GET EMPLOYEE INPUTSs
+        strMDMSID = request.form['EmployeeID']
         strMDMLName = request.form['LastName']
         strMDMFName = request.form['FirstName']
         strMDMMName = request.form['MiddleName']
         strMDMGender = request.form['gender']
-        strMDMBirthday = request.form['Birthday']
+        strMDMDateHired = request.form['DateHired']
         strMDMAge = request.form['Age']
-        strMDMCourse = request.form['Course']
+        strMDMDepartment = request.form['Department']
         strMDMHomeAdd = request.form['Address']
         strMDMEmailAdd = request.form['EmailAdd']
 
         if request.method == 'POST':
             # SAVE RECORD TO DATABASE
-            sql = "UPDATE tbl_stud_info SET \
+            sql = "UPDATE tbl_emp_info SET \
                     Last_Name=%s, First_Name=%s, Middle_Name=%s, Gender=%s, \
-                    Birthday=%s, Age=%s, Course=%s,Home_Address=%s, Email_Address=%s \
-                    WHERE Student_ID=%s"
-            data = (strMDMLName, strMDMFName, strMDMMName, strMDMGender, strMDMBirthday,
-                    strMDMAge, strMDMCourse, strMDMHomeAdd, strMDMEmailAdd, strMDMSID)
+                    Date_Hired=%s, Age=%s, Department=%s,Home_Address=%s, Email_Address=%s \
+                    WHERE Employee_ID=%s"
+            data = (strMDMLName, strMDMFName, strMDMMName, strMDMGender, strMDMDateHired,
+                    strMDMAge, strMDMDepartment, strMDMHomeAdd, strMDMEmailAdd, strMDMSID)
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
             conn.commit()
             flash('Record successfully edited in the database!')
-            return redirect('/read_all_stud')
+            return redirect('/read_all_emp')
         else:
-            return 'Error while updating student'
+            return 'Error while updating employee'
     except Exception as e:
         print(e)
     finally:
         cursor.close()
         conn.close()
 
-@app.route('/delete_stud/<string:id>')
-def delete_stud(id):
+@app.route('/delete_emp/<string:id>')
+def delete_emp(id):
     conn = None
     cursor = None
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM tbl_stud_info WHERE student_id=%s", (id,))
+        cursor.execute("DELETE FROM tbl_emp_info WHERE employee_id=%s", (id,))
         conn.commit()
         flash('Record successfully deleted from the server!')
-        return redirect('/read_all_stud')
+        return redirect('/read_all_emp')
     except Exception as e:
         print(e)
     finally:
